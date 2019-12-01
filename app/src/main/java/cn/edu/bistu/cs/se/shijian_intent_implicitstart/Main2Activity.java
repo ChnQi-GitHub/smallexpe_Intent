ae31330c4e9417e3f4c00b7e784b942766a98f2b
lyt <1003655329@qq.com>
2019-12-01 17:14:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/shijian_intent_implicitstart/Main2Activity.java
@@ -0,0 +1,14 @@
+package cn.edu.bistu.cs.se.shijian_intent_implicitstart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
